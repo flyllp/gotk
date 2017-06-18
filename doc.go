1be17e2bb4971e4e61c1d51245a7c3f0b6fcd1d3
flyllp <flyllp@126.com>
2017-06-18 17:23:23 +0800

    Hello World!

---
+++ doc.go
@@ -0,0 +1,6 @@
+// gotk project doc.go
+
+/*
+gotk document
+*/
+package main
---
+++ main.go
@@ -0,0 +1,10 @@
+// gotk project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
